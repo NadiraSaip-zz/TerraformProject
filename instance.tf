0e66d191430d8ba2c8f1461dc66a9f957cdacbed
NadiraSaip <nadiasaip@gmail.com>
2018-11-01 02:34:55 +0000

    Terraform ket for jenkins

--- instance.tf
+++ instance.tf
@@ -7,7 +7,24 @@ resource "aws_instance" "instance1" {
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("apache.sh")}"
   associate_public_ip_address = "true"
-}
+
+   tags {
+    Name = "{var.appname}"
+    Enviroment = "${var.Enviroment}"
+    Created_by = "{var.Created_by}"
+} 
+
+lifecycle {
+  
+   tags {
+    Name = "{var.appname}"
+    Enviroment = "${var.Enviroment}"
+    Created_by = "{var.Created_by}"
+    prevent_destroy = "false"
+  }
+ }
+ 
+
 #DB host
 resource "aws_instance" "instance2" {
   ami                    = "${lookup(var.amis, var.region)}"
@@ -17,4 +34,12 @@ resource "aws_instance" "instance2" {
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("db.sh")}"
 }
-
+   tags {
+    Name = "{var.appname}"
+    Enviroment = "${var.Enviroment}"
+    Created_by = "{var.Created_by}"
+}
+lifecycle {
+   prevent_destroy = "false"
+}
+}
